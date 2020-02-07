3d09ca5f7203facdad0318ba293036e44be736a2
pnk4 <patrick.kamongi@nist.gov>
2020-02-07 17:37:44 -0500

    Forecast accuracy measure added, needs to be tested further.

--- notebooks/rot_helper/lstm_forecast.py
+++ notebooks/rot_helper/lstm_forecast.py
@@ -2,7 +2,12 @@
 import numpy as np
 import tensorflow as tf
 
+from .ssd_detection import detection_data, post_process_detection_data
+
 DEBUG=False
+seq_num             = 50
+num_bboxes          = 5
+max_bboxes_vec_size = 6 * num_bboxes # About 6 points data for each of the 5 bboxes per an image.
 
 def local_padding_series(ssd_detection_data, max_bboxes_vec_size):
     """ Given an ssd_detection_data, return its padded version """
@@ -209,4 +214,93 @@ def longterm_forecast_report_on_occlusions_predictions_area(forecast_results):
         print("\n\n:: Long Term Forecast Report End.")
 
     return list((np.array(forecast_booleanist_final)).flatten())
+
+def forecasted_occlusion_position(occlusion_bool_list):
+    """ Given a boolean list of occlusion events, return the index position of the forecasts
+        that indicate that an occlusion event has been predicted. """
+    
+    positional_list = []
+    
+    for i in range(len(occlusion_bool_list)):
+        if occlusion_bool_list[i] == "Yes":
+            positional_list.append(i)
+    
+    return positional_list
  
+def one_forecast_accuracy(candidate_key, forecast_report):
+    """ Given a next frame forecast report, and next candidate image, compute the actual/predicted occlusion accuracy """
+
+    category_boolean = {"No":0, "Yes":1}
+
+    # Given a candidate next image (candidate_key), compute its SSD detection bbox data    
+    print("\n:: Candidate Key-1: ", candidate_key)
+
+    # Run SSD Detection
+    results_detection_data = detection_data(candidate_key)
+    ssd_detection_data = post_process_detection_data(results_detection_data)
+
+    candidate_ssd_data = list(ssd_detection_data.values())[0]    
+    #print("\n:: candidate_ssd_data: ", candidate_ssd_data)
+
+    padded_candidate_ssd_data = post_process_ssd_detection_data_for_forecasting(candidate_ssd_data, max_bboxes_vec_size)
+    #print("\n:: padded_candidate_ssd_data: ", padded_candidate_ssd_data)
+
+    # forecast_report_on_occlusions_predictions_area function has been re-purposed here.
+    detected_bool = category_boolean[forecast_report_on_occlusions_predictions_area(padded_candidate_ssd_data)[0]]
+
+    #print("\n:: forecast_report: ", forecast_report)
+    predicted_bool = category_boolean[forecast_report[0]]
+
+    #print("\n:: predicted_bool: ", predicted_bool)
+    #print("\n:: detected_bool: ", detected_bool)
+
+    # Compute its mse aggr. value.
+    accuracy_1_eval = accuracy_score([predicted_bool], [detected_bool])
+
+    return accuracy_1_eval
+
+def multi_forecast_accuracy(candidate_key, next_n_frames_forecasts_report, forecast_window):
+    """ This assume that we access to n+1 future images access to validate the 
+        LSTM forecasts report of the next n-forecast_window sky images ssd detecton data. """
+    
+    ssd_detection_data_list = []
+    candidate_filenames = []
+    candidate_key_filename = candidate_key.split("/")[-1]
+    for i in range(len(sky_camera_images_files)):
+        if sky_camera_images_files[i] == candidate_key_filename:
+            candidate_filenames = sky_camera_images_files[i:i+forecast_window]
+            break
+    #print("\n:: candidate_filenames-n: ", len(candidate_filenames), "\n", candidate_filenames)
+    
+    
+    for image_filename in candidate_filenames:
+        
+        candidate_filepath = os.path.join(sky_camera_images_dir, image_filename)
+
+        # Run SSD Detection
+        results_detection_data = detection_data(candidate_filepath)
+        ssd_detection_data = post_process_detection_data(results_detection_data)
+        candidate_ssd_data = list(ssd_detection_data.values())[0]  
+        padded_candidate_ssd_data = post_process_ssd_detection_data_for_forecasting(candidate_ssd_data, max_bboxes_vec_size)
+        
+        #print("\n:: padded_candidate_ssd_data: ", padded_candidate_ssd_data)
+        
+        ssd_detection_data_list.append(padded_candidate_ssd_data)
+    
+    ssd_detection_data_list = np.asarray(ssd_detection_data_list)
+    #print("\n\n:: ssd_detection_data_list: ", len(ssd_detection_data_list), ssd_detection_data_list, "\n")
+    
+    # longterm_forecast_report_on_occlusions_predictions_area function has been re-purposed here.
+    detection_bool_list = longterm_forecast_report_on_occlusions_predictions_area(ssd_detection_data_list)
+    
+    print("\n:: detection_bool_list: ", len(detection_bool_list), detection_bool_list)
+    print("\n\n:: next_n_frames_forecasts_report: ", len(next_n_frames_forecasts_report), next_n_frames_forecasts_report, "\n")
+    
+    #print("\n\n:: next_n_frames_forecasts[0]: ", len(next_n_frames_forecasts[0]), next_n_frames_forecasts[0], "\n")
+    #print(len(ssd_detection_data_list), len(next_n_frames_forecasts))
+    #print("\nCheck Mate: ", ssd_detection_data_list[0], next_n_frames_forecasts[0], "\n")
+
+    # Compute its mse aggr. value.
+    accuracy_multi_eval = accuracy_score(next_n_frames_forecasts_report, detection_bool_list)
+    
+    return accuracy_multi_eval
